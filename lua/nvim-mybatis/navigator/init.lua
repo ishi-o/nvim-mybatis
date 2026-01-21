e749a01bd105fc7a6f0327d90e5326612f0aadda
Ishio <458457289@qq.com>
2026-01-21 13:19:41 +0800

    refactor(navigator): rename xml2java and java2xml module

--- lua/nvim-mybatis/navigator/init.lua
+++ lua/nvim-mybatis/navigator/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.xml2java = require("nvim-mybatis.navigator.xml2java")
-M.java2xml = require("nvim-mybatis.navigator.java2xml")
+M.xml = require("nvim-mybatis.navigator.xml")
+M.java = require("nvim-mybatis.navigator.java")
 
 return M
