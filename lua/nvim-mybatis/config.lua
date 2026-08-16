dd61d004d5811220b5c30c62a75d9e081066d1fe
Ishio <458457289@qq.com>
2026-08-17 00:40:43 +0800

    fix workflow and setup

--- lua/nvim-mybatis/config.lua
+++ lua/nvim-mybatis/config.lua
@@ -49,13 +49,7 @@ M.values = DEFAULT_CONFIG
 
 --- @param config mybatis.NvimMybatisConfig?
 function M.setup(config)
-	-- reset to defaults in place (keeping the table identity) so references
-	-- captured by modules at require time stay live, and repeated setup()
-	-- calls don't compound
-	for key in pairs(M.values) do
-		M.values[key] = nil
-	end
-	vim.tbl_deep_extend("force", M.values, vim.deepcopy(DEFAULT_CONFIG), config or {})
+	vim.tbl_deep_extend("force", M.values, config or {})
 	return M
 end
 
