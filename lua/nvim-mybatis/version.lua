aeba2df85fa78dd0bae2f821dc783eceecd9eea7
Ishio <458457289@qq.com>
2026-08-21 15:52:56 +0800

    chore: update version

--- lua/nvim-mybatis/version.lua
+++ lua/nvim-mybatis/version.lua
@@ -1,6 +1,6 @@
 --- Single source of truth for the plugin version.
 --- The release workflow reads this file to create the `v<version>` tag automatically.
 --- @return string
-local VERSION = "0.4.1"
+local VERSION = "0.4.2"
 
 return VERSION
